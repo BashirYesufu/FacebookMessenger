bb76e7af45b78877ce18c7bc50e798abc8b371af
Bashir Yesufu <86322263+BashirYesufu@users.noreply.github.com>
2021-10-09 01:31:43 +0100

    Layed out the logo, email and password textfields

---
+++ Messenger/Messenger/Controllers/Login-Register/LoginViewController.swift
@@ -0,0 +1,95 @@
+//
+//  LoginViewController.swift
+//  Messenger
+//
+//  Created by Decagon Macbook Pro on 08/10/2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.clipsToBounds = true
+        return scrollView
+    }()
+    
+    private let logo: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "logo")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let emailField: UITextField = {
+        let textField = UITextField()
+        textField.autocapitalizationType = .none
+        textField.autocorrectionType = .no
+        textField.returnKeyType = .continue
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.lightGray.cgColor
+        textField.layer.cornerRadius = 12
+        textField.placeholder = "Email Address ..."
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        textField.leftViewMode = .always
+        textField.backgroundColor = .white
+        return textField
+    }()
+    
+    private let passwordField: UITextField = {
+        let textField = UITextField()
+        textField.autocapitalizationType = .none
+        textField.autocorrectionType = .no
+        textField.returnKeyType = .done
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.lightGray.cgColor
+        textField.layer.cornerRadius = 12
+        textField.placeholder = "Password ..."
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        textField.leftViewMode = .always
+        textField.backgroundColor = .white
+        textField.isSecureTextEntry = true
+        return textField
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Log In"
+        view.backgroundColor = .white
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Register",
+                                                                                  style: .done,
+                                                                                  target: self,
+                                                                                  action: #selector(goToRegister))
+        view.addSubview(scrollView)
+        scrollView.addSubview(logo)
+        scrollView.addSubview(emailField)
+        scrollView.addSubview(passwordField)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        scrollView.frame = view.bounds
+        let size = scrollView.width/3
+        logo.frame = CGRect(x: (scrollView.width - size)/2,
+                            y: 20,
+                            width: size,
+                            height: size)
+        
+        emailField.frame = CGRect(x: 30,
+                                  y: logo.bottom + 10,
+                                  width: scrollView.width - 60,
+                                  height: 52)
+        
+        passwordField.frame = CGRect(x: 30,
+                                     y: emailField.bottom + 10,
+                                     width: scrollView.width - 60,
+                                     height: 52)
+    }
+    
+    @objc private func goToRegister() {
+        let viewController = RegisterViewController()
+        navigationController?.pushViewController( viewController, animated: true)
+    }
+    
+}
---
+++ Messenger/Messenger/Controllers/Login-Register/RegisterViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterViewController.swift
+//  Messenger
+//
+//  Created by Decagon Macbook Pro on 08/10/2021.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Register"
+        view.backgroundColor = .white
+    }
+    
+
+}
