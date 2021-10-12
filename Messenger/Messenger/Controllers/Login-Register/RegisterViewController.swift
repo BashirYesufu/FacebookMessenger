37fa1244e1dbbf533ceeb6a9f88b7434ecd417e1
Bashir Yesufu <86322263+BashirYesufu@users.noreply.github.com>
2021-10-13 00:38:06 +0100

    Implemented Account creation in firebase

--- Messenger/Messenger/Controllers/Login-Register/RegisterViewController.swift
+++ Messenger/Messenger/Controllers/Login-Register/RegisterViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
     
@@ -165,6 +166,11 @@ class RegisterViewController: UIViewController {
                   return
               }
         // firebase register
+        FirebaseAuth.Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            guard let result = authResult, error == nil else { return }
+            let user = result.user
+            print("Created User: \(user)")
+        }
     }
     
     func alertUserLoginError() {
